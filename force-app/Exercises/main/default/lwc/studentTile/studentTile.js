8a651ac9b77fdbffad25e650b69fab13b1614b75
MadhuriReddy30 <45865342+MadhuriReddy30@users.noreply.github.com>
2020-09-14 14:34:21 -0700

    Exercise 2-4 Completed

---
+++ force-app/Exercises/main/default/lwc/studentTile/studentTile.js
@@ -0,0 +1,16 @@
+import { LightningElement, api } from 'lwc';
+
+export default class StudentTile extends LightningElement {
+
+  @api student = {
+    Name: 'Madhuri',
+    PhotoUrl: '/services/images/photo/003B0FakePictId',
+    };
+
+    @api selected = false;
+
+
+    get tileSelected() {
+      return this.selected ? "tile selected " : "tile";
+      }
+}
