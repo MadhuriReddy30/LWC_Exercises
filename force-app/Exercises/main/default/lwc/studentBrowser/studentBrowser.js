1185af14c75e67dc1d83ffe39634363df7a40443
MadhuriReddy30 <45865342+MadhuriReddy30@users.noreply.github.com>
2020-09-15 12:08:01 -0700

    Exercise 2-10 Completed

--- force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
+++ force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
@@ -1,5 +1,7 @@
 import { LightningElement, track, wire } from 'lwc';
 import getStudents from '@salesforce/apex/StudentBrowser.getStudents';
+import { publish, MessageContext } from 'lightning/messageService';
+import SELECTED_STUDENT_CHANNEL from '@salesforce/messageChannel/SelectedStudentChannel__c';
 export default class StudentBrowser extends LightningElement {
 
 
@@ -13,8 +15,23 @@ export default class StudentBrowser extends LightningElement {
   selectedDeliveryId = '';
   selectedInstructorId = '';
 
-  handleFilterChange(event){
+  handleFilterChange(event) {
     this.selectedDeliveryId = event.detail.deliveryId;
     this.selectedInstructorId = event.detail.instructorId;
+  }
+
+  @wire(MessageContext) messageContext;
+
+  handleStudentSelected(event) {
+    const studentId = event.detail.studentId;
+    this.updateSelectedStudent(studentId);
+  }
+
+  updateSelectedStudent(studentId) {
+    const message = {
+      studentId: studentId
     }
+    publish(this.messageContext, SELECTED_STUDENT_CHANNEL,
+      message);
+  }
 }
